2e042e8dbe79c17bb82276d8396486207ef88234
fromabby <60252891+fromabby@users.noreply.github.com>
2022-03-19 22:43:16 +0800

    Update Wordle.js

--- src/components/wordle/Wordle.js
+++ src/components/wordle/Wordle.js
@@ -8,11 +8,11 @@ const Wordle = ({ word }) => {
         wordList.push(word[i])
     }
 
-    const [firstRow, setFirstRow] = useState(['','','','',''])
-    const [secondRow, setSecondRow] = useState(['','','','',''])
-    const [thirdRow, setThirdRow] = useState(['','','','',''])
-    const [fourthRow, setFourthRow] = useState(['','','','',''])
-    const [fifthRow, setFifthRow] = useState(['','','','',''])
+    const [firstRow, setFirstRow] = useState(['', '', '', '', ''])
+    const [secondRow, setSecondRow] = useState(['', '', '', '', ''])
+    const [thirdRow, setThirdRow] = useState(['', '', '', '', ''])
+    const [fourthRow, setFourthRow] = useState(['', '', '', '', ''])
+    const [fifthRow, setFifthRow] = useState(['', '', '', '', ''])
 
     console.log(firstRow)
     return (
@@ -31,36 +31,3 @@ const Wordle = ({ word }) => {
 }
 
 export default Wordle
-
-
-    // const [firstFirst, setFirstFirst] = useState('')
-    // const [secondFirst, setSecondFirst] = useState('')
-    // const [thirdFirst, setThirdFirst] = useState('')
-    // const [fourthFirst, setFourthFirst] = useState('')
-    // const [fifthFirst, setFifthFirst] = useState('')
-
-    // const [firstSecond, setFirstSecond] = useState('')
-    // const [secondSecond, setSecondSecond] = useState('')
-    // const [thirdSecond, setThirdSecond] = useState('')
-    // const [fourthSecond, setFourthSecond] = useState('')
-    // const [fifthSecond, setFifthSecond] = useState('')
-
-    // const [firstThird, setFirstThird] = useState('')
-    // const [secondThird, setSecondThird] = useState('')
-    // const [thirdThird, setThirdThird] = useState('')
-    // const [fourthThird, setFourthThird] = useState('')
-    // const [fifthThird, setFifthThird] = useState('')
-
-    // const [firstFourth, setFirstFourth] = useState('')
-    // const [secondFourth, setSecondFourth] = useState('')
-    // const [thirdFourth, setThirdFourth] = useState('')
-    // const [fourthFourth, setFourthFourth] = useState('')
-    // const [fifthFourth, setFifthFourth] = useState('')
-
-    // const [firstFifth, setFirstFifth] = useState('')
-    // const [secondFifth, setSecondFifth] = useState('')
-    // const [thirdFifth, setThirdFifth] = useState('')
-    // const [fourthFifth, setFourthFifth] = useState('')
-    // const [fifthFifth, setFifthFifth] = useState('')
-
-
